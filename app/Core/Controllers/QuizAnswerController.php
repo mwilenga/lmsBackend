f4c72683438875afb91bbf4a0c47e25aa415c592
Festus Mwilenga <flowinfestus@gmail.com>
2026-03-02 10:02:34 +0300

    UPDATE 20260302 | Fix issue

--- app/Core/Controllers/QuizAnswerController.php
+++ app/Core/Controllers/QuizAnswerController.php
@@ -140,7 +140,7 @@ class QuizAnswerController
                 case FormMethod::get('SAVE/value') :
 
                     $output = $this->quizanswerService->transaction(function () use ($request) {
-                        info('REQUEST DATA', ['REQUEST DATA' => $request]);
+                        // info('REQUEST DATA', ['REQUEST DATA' => $request]);
                         $listOfAnswer = $request->answers;
                         foreach ($listOfAnswer as $answer) {
                             $request['uuid'] = Str::uuid();
